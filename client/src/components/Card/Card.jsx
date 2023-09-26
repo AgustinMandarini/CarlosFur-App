df5b5a90618912dad7f2efb14c765d54cf45d21a
Marianojgiusti <giusti_mariano@hotmail.com>
2023-09-26 20:55:37 -0300

    CreacionComponenteCard2

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ const Card = (props) =>{
     return (
         <div className="card-container" key={props.id}>
             <h1>nombre: {props.nombre}</h1>
-            <p>tipo de madera: {props.tipo_madera}</p>
+            <p>tipo de maderas: {props.tipo_madera}</p>
         </div>
     ) 
     }

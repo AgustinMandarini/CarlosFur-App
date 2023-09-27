74cf44e013a7a7dededde107312916e8d2e29264
Miliros <milagros4046@hotmail.com>
2023-09-27 11:28:12 -0300

    agregue los componentes a su ruta

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./views/Nav/NavBar";
 import About from "./views/About/About";
+import Home from "./views/Home/Home";
+import Detail from "./views/Home/Detail";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/home" />
-        <Route path="/detail/:detailId" />
+        <Route path="/home" Component={<Home />} />
+        <Route path="/detail/:detailId" Component={<Detail />} />
         <Route path="/create" />
         <Route path="/about" Component={<About />} />
       </Routes>

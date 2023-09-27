86cf2a24bea79e476fb686d85ba8cfa47ca993ed
javimana <jpereyra1979@gmail.com>
2023-09-26 21:48:21 -0300

    EPICA.BackEsenciales.HU.indexjs

--- server/index.js
+++ server/index.js
@@ -7,4 +7,13 @@
 // server.listen(PORT, () => {//se inicia el servidor
 //   console.log(`Server listening on port ${PORT}`);
 // })
-console.log("algo");
+
+const server = require("./src/app.js");
+const { conn, conexion } = require("./src/db.js");
+
+// Syncing all the models at once.
+conexion.sync({ alter: true }).then(() => {
+  server.listen(3001, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  });
+});

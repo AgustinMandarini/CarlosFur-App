e8982e1e38bdc0dd46108200cc7825fd2da77ee6
Miliros <milagros4046@hotmail.com>
2023-09-27 14:50:17 -0300

    Agregue import de useParams

--- client/src/views/Home/Detail.jsx
+++ client/src/views/Home/Detail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./Detail.module.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Detail = () => {
   const { id } = useParams();

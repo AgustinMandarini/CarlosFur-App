192b0c680770ab0e80bc4bb414955807547f619d
Kobi96 <tobiasolveira@outlook.com>
2023-09-28 14:26:43 -0300

    CORRECCION.muebles.json

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,14 +1,14 @@
 import CardsContainer from "../../components/CardsContainer/Cardscontainer";
 import React from "react";
+import muebles from "../../muebles.json";
 
 const Home = () => {
-  return(
-     <div>
-    <h1>Volvieron las Sillas 2x1!</h1>
-    <CardsContainer></CardsContainer>
-  </div>
-  )
+  return (
+    <div>
+      <h1>Volvieron las Sillas 2x1!</h1>
+      <CardsContainer></CardsContainer>
+    </div>
+  );
 };
-
 
 export default Home;

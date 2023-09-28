68b8ca7ef599a39a91f7c004e7358d00ea030447
Marianojgiusti <giusti_mariano@hotmail.com>
2023-09-28 01:48:53 -0300

    terminando home, cartas hardcodeadas -Mariano

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,8 +1,10 @@
 import CardsContainer from "../../components/CardsContainer/Cardscontainer";
 import React from "react";
+
 const Home = () => {
-  return( <div>
-    <h1>Vistaaa home</h1>
+  return(
+     <div>
+    <h1>Volvieron las Sillas 2x1!</h1>
     <CardsContainer></CardsContainer>
   </div>
   )

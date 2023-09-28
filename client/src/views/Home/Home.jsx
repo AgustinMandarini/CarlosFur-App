7155790a2b610d18ff69677e821e7ff2cba87408
Kobi96 <tobiasolveira@outlook.com>
2023-09-28 14:39:01 -0300

    CORRECIÓN.importación-CardsContainer

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,11 +1,28 @@
 import CardsContainer from "../../components/CardsContainer/Cardscontainer";
 import React from "react";
-import muebles from "../../muebles.json";
+import { useState } from "react";
+import mueblesData from "../../muebles.json";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Home = () => {
+  const [muebles, setMuebles] = useState(mueblesData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [mueblesPerPage] = useState(5);
+
+  const indexOfLastRecipe = currentPage * mueblesPerPage;
+  const indexOfFirstRecipe = indexOfLastRecipe - mueblesPerPage;
+  const currentMuebles = muebles.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div>
       <h1>Volvieron las Sillas 2x1!</h1>
+      <Pagination
+        recipesPerPage={mueblesPerPage}
+        totalRecipes={muebles.length}
+        paginate={paginate}
+      />
       <CardsContainer></CardsContainer>
     </div>
   );

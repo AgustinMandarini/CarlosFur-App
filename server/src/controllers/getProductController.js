46d5cee3dfa2ea44acbed4db585d2dd8b876e721
delfinaspais <delfina.spais@gmail.com>
2023-09-28 17:06:21 -0300

    Creacion busqueda ByName

--- server/src/controllers/getProductController.js
+++ server/src/controllers/getProductController.js
@@ -1,10 +1,23 @@
 const { Product } = require("../db.js");
+const { Op } = require("sequelize");
 
-const findAllProducts = async () => {
-  const allProductsArray = await Product.findAll();
-  return allProductsArray;
+const findAllProducts = async (name) => {
+  if (name) {
+    const productsByName = await Product.findAll({
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`,
+        },
+      },
+    });
+    return productsByName;
+  } else {
+   
+    const allProductsArray = await Product.findAll();
+    return allProductsArray;
+
+  }
 };
 
-module.exports = {
-  findAllProducts,
-};
+module.exports = { findAllProducts };
+

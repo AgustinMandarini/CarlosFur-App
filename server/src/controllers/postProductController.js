906fba3cb2f21124dbe7bb6d84770044902821f1
RMGeorgetti <rodrigogeorgetti.cba@gmail.com>
2023-09-30 19:44:12 -0300

    actualizacion local

--- server/src/controllers/postProductController.js
+++ server/src/controllers/postProductController.js
@@ -1,9 +1,9 @@
 const { Product, ProductType } = require("../db");
 const { findAllTypes } = require("./getProductTypeController");
 
-const postProductController = async ({name,price,height,depth,width,weight,color,description,productType}) => {
+const postProductController = async ({name,price,height,depth,width,weight,color,description,productTypeId}) => {
   // Crea un nuevo producto, sin agregar aun el tipo de producto, que sera una relacion manyToMany con la tabla productType
-  const newProduct = await Product.create({name,price,height,depth,width,weight,color,description});
+  const newProduct = await Product.create({name,price,height,depth,width,weight,color,description,productTypeId});
 
   // Este codigo asocia un tipo de producto a un producto
 
@@ -13,12 +13,14 @@ const postProductController = async ({name,price,height,depth,width,weight,color
   //   })
   // );
 
-  await findAllTypes();
-  const type = await ProductType.findOne({
-    where: { name: productType },
-  });
-  await newProduct.setProductType(type);
-  return newProduct;
+  // await findAllTypes();
+  // const type = await ProductType.findOne({
+  //   where: { name: productType },
+  // });
+  
+  // await newProduct.setProductType(type);
+return newProduct
+  
 };
 
 module.exports = { postProductController };

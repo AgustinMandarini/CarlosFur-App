6044d4cc779452082bbdd1b327a10128ba5f41ab
Miliros <milagros4046@hotmail.com>
2023-10-01 12:42:46 -0300

    buttons boostrap

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,6 +3,7 @@ import style from "./SearchBar.module.css";
 import { useLocation } from "react-router-dom";
 import { getMuebleName } from "../../redux/actions";
 import { useDispatch } from "react-redux";
+import { InputGroup, Form, Button } from "react-bootstrap";
 
 const SearchBar = () => {
   const location = useLocation();
@@ -25,25 +26,17 @@ const SearchBar = () => {
   // }, []);
 
   return (
-    <div>
+    <div className={style.divSerchBar}>
       {location.pathname === "/home" && (
-        <div className={style.divSerchBar}>
-          <input
-            type="search"
+        <InputGroup size="sm">
+          <Form.Control
             placeholder="buscar mueble..."
-            value={mueble}
             onChange={handleSubmit}
-            className={style.inputSearch}
           />
-          <button
-            type="submit"
-            className={style.addButton}
-            onClick={handleOnClick}
-          >
-            {" "}
+          <Button className="ms-1" variant="dark" onClick={handleOnClick}>
             Buscar
-          </button>
-        </div>
+          </Button>
+        </InputGroup>
       )}
     </div>
   );
--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import style from "./LandingPage.module.css";
 
 const LandingPage = () => {
@@ -6,9 +6,9 @@ const LandingPage = () => {
     <div className={style.background}>
       <div className={style.container}>
         <h1 className={style.tittle}> MSC Amoblamientos</h1>
-        <NavLink to="/home" className={style.loginButton}>
+        <Button variant="dark" href="/home" size="sm">
           Ingresar
-        </NavLink>
+        </Button>
       </div>
     </div>
   );

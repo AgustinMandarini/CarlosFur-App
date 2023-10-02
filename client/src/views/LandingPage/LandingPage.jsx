f2589f26676a35b47126c17df96e9193feb2adbd
fgarciamatar <fgarciamatar@gmail.com>
2023-10-02 13:57:46 -0300

    landinpage

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,13 +1,14 @@
 import { Button } from "react-bootstrap";
 import style from "./LandingPage.module.css";
+import imagen from "./../../imagenes/icono.png"
 
 const LandingPage = () => {
   return (
     <div className={style.background}>
       <div className={style.container}>
-        <h1 className={style.tittle}> MSC AMOBLAMIENTOS</h1>
-        <Button variant="dark" href="/home" size="sm">
-          Ingresar
+        <img src={imagen} alt="ICONO MSC" width="10px" />
+        <Button variant="dark" href="/home" size="sm" className={style.boton}>
+          Ingresar a la Tienda
         </Button>
       </div>
     </div>

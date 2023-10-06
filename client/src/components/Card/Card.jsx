20ff4bf8291fc30a44f0c3874f2724d5b73c3f9d
Kobi96 <tobiasolveira@outlook.com>
2023-10-06 10:47:55 -0300

    Contador

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import style from "./Card.module.css";
-import imagenDefault from "./../../imagenes/default.png";
+import defaultImage from "./../../imagenes/default.png";
 
 const Card = (props) => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decreaseCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className={style.container} key={props.id}>
       <Link to={`/detail/${props.id}`} className={style.nameLink}>
@@ -12,7 +24,7 @@ const Card = (props) => {
             <img src={props.imagePath} alt="image" className={style.imgCard} />
           </>
         ) : (
-          <img src={imagenDefault} alt="imagen default" />
+          <img src={defaultImage} alt="default image" />
         )}
         <h1 className={style.nameCard}>{props.name}</h1>
         <div className={style.divProps}>
@@ -20,6 +32,11 @@ const Card = (props) => {
           <p className={style.description}>{props.description} </p>
         </div>
       </Link>
+      <div className={style.counterContainer}>
+        <button onClick={decreaseCounter}>-</button>
+        <span className={style.counterValue}>{counter}</span>
+        <button onClick={increaseCounter}>+</button>
+      </div>
     </div>
   );
 };

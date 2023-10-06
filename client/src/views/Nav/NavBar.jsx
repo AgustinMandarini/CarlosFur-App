7c57917b7e9969a94cc677bbb36c0d952789d10b
Kobi96 <tobiasolveira@outlook.com>
2023-10-05 22:29:54 -0300

    Creación de view Carrito y logo en NavBar

--- client/src/views/Nav/NavBar.jsx
+++ client/src/views/Nav/NavBar.jsx
@@ -39,33 +39,6 @@ const NavBar = () => {
             id="basic-navbar-nav"
             className={navResponsive ? style.active : ""}
           >
-            <Nav className="ms-auto">
-              <div
-                className={
-                  navResponsive ? style.divLinksResponsive : style.divLinks
-                }
-              >
-                <Link
-                  to="/home"
-                  className={navResponsive ? style.linkResponsive : style.links}
-                  onClick={() => dispatch(getProducts())}
-                >
-                  Home
-                </Link>
-                <Link
-                  to="/create"
-                  className={navResponsive ? style.linkResponsive : style.links}
-                >
-                  Form
-                </Link>
-                <Link
-                  to="/about"
-                  className={navResponsive ? style.linkResponsive : style.links}
-                >
-                  About
-                </Link>
-              </div>
-            </Nav>
             <div
               className={
                 navResponsive ? style.searchBarResponsive : style.searchBar
@@ -105,10 +78,10 @@ const NavBar = () => {
                   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                 </svg>
               </Link>
-              <SearchBar />
             </div>
           </Navbar.Collapse>
         </Container>
+        <SearchBar />
       </Navbar>
     </>
   );

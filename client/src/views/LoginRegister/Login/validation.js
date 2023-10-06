c561182608d38ff2c6b165e4748774500a090d71
Chwaksen <agustinmandarini47@gmail.com>
2023-10-06 17:37:47 -0300

    Estilos de Javi aplicadons a auth0 de Agus

--- client/src/views/LoginRegister/Login/validation.js
+++ client/src/views/LoginRegister/Login/validation.js
@@ -14,11 +14,6 @@ const validation = (formState) => {
 
   if (!formState.password) {
     errors.password = "Ingrese una contraseña";
-  } else if (formState.password.length < 6 || formState.password.length > 10) {
-    errors.password = "Debe tener entre 8 y 12 caracteres";
-  } else if (!regexPassword.test(formState.password)) {
-    errors.password =
-      "Debe tener un caracter especial, una mayuscula y un número";
   }
   return errors; // Devolver el objeto de errores
 };

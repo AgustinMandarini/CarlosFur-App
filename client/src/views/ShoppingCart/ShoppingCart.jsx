fd37bd47a0b5fcceaa9b7c92845ebffd5155b13b
fgarciamatar <fgarciamatar@gmail.com>
2023-10-08 18:15:31 -0300

    cambios

--- client/src/views/ShoppingCart/ShoppingCart.jsx
+++ client/src/views/ShoppingCart/ShoppingCart.jsx
@@ -1,6 +1,19 @@
-import style from "./ShoppingCart.module.css";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import CartProductContainer from "../../components/CartProductContainer/CartProductContainer";
+import style from "./ShoppingCart.module.css";
 const ShoppingCart = () => {
+
+ const stateCartProducts =  useSelector((state)=> state.cartProducts);
+//  console.log(stateCartProducts);
+ const updateLocalStorage = (cart) => {
+  const carrito = [];
+carrito = [...carrito, cart];
+  localStorage.setItem('cart', JSON.stringify(carrito));
+};
+ useEffect(() => {
+  updateLocalStorage(stateCartProducts);
+}, [stateCartProducts]);
   return (
     <div className={style.background}>
       <CartProductContainer />

3d686136de860e50e6bcfd8bdb6e6ef687535ec7
Kobi96 <tobiasolveira@outlook.com>
2023-10-08 19:43:23 -0300

    cambios

--- client/src/views/ShoppingCart/ShoppingCart.jsx
+++ client/src/views/ShoppingCart/ShoppingCart.jsx
@@ -1,6 +1,17 @@
-import style from "./ShoppingCart.module.css";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import CartProductContainer from "../../components/CartProductContainer/CartProductContainer";
+import style from "./ShoppingCart.module.css";
 const ShoppingCart = () => {
+  const stateCartProducts = useSelector((state) => state.cartProducts);
+  const [carrito, setCarrito] = useState([]);
+  const updateLocalStorage = (cart) => {
+    const carro = [...carrito, cart];
+    localStorage.setItem("cart", JSON.stringify(carro));
+  };
+  useEffect(() => {
+    updateLocalStorage(stateCartProducts);
+  }, [stateCartProducts]);
   return (
     <div className={style.background}>
       <CartProductContainer />

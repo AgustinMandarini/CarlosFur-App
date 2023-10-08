5f3c8dabb50782ee6b7e5ca4d59aa9c3a8916f8a
delfinaspais <delfina.spais@gmail.com>
2023-10-08 13:04:19 -0300

    Modificaciones en cartController, cartHandler, model/Cart

--- server/src/controllers/cartController.js
+++ server/src/controllers/cartController.js
@@ -1,71 +1,71 @@
 const { Cart, Product } = require('../db');
-
-const getCart = async () => {
-  try {
-
-    const carts = await Cart.findAll();
-    return carts;
-
-  } catch (error) {
-    console.error('Error in getCart:', error);
-    throw error; // Error al handler
-  }
-};
+const Sequelize = require('sequelize');
 
 const createCart = async (req) => {
   try {
-
-    // const { userId, products } = req.body;
-    // try {
-    //   const user = await User.findByPk(userId);
-    //   if (!user) {
-    //     throw new Error("User not found");
-    //   }
-
-    const { products, sale_date } = req.body;
+    const { products } = req.body;
 
     if (!Array.isArray(products) || products.length === 0) {
       return { status: 400, data: { error: 'Invalid or empty product list' } };
     }
 
     
-    
     const productIds = products.map((product) => product.id);
     
-    const databaseProducts = await Product.findAll({
-        where: { id: productIds },
-        attributes: ['id', 'price']
+      const databaseProducts = await Product.findAll({
+      where: { id: productIds },
     });
-    
-    const totalAmount = databaseProducts.reduce((total, product) => {
-        
-    return total + (product.price * product.quantity);
-    }, 0.0); 
 
-    const filteredProductIds = productIds.filter(
-      (productId) => !databaseProducts.some((product) => product.id === productId)
-    );
+    console.log("AAAA", databaseProducts)
 
-    if (filteredProductIds.length > 0) {
-      return {
-        status: 400,
-        data: { error: `The following product IDs do not exist: ${filteredProductIds.join(', ')}` },
-      };
+    let totalAmount = 0.0;
+    for (const product of products) {
+      const databaseProduct = databaseProducts.find((dbProduct) => dbProduct.id === product.id);
+      if (!databaseProduct) {
+        continue; 
+      }
+      totalAmount += databaseProduct.price * product.quantity;
     }
 
-    const cart = await Cart.create({ 
-      sale_date: sale_date || new Date(), // metodo para obtener la fecha actual si no se proporciona
-      total_amount: totalAmount,
-      products: products 
+      const cart = await Cart.create({ 
+        total_amount: totalAmount, // Total calculado
     });
-
-    return { status: 201, data: cart };
+    
+      await cart.addProducts(products.map((product) => product.id), { // Asocia los productos con sus cantidades al carrito
+      through: { quantity: Sequelize.col('quantity') }, // Asocia la cantidad del producto
+    });
+    
+      const updatedCart = await Cart.findByPk(cart.id, {
+      include: [{ model: Product, as: 'products' }],
+    });
+    
+    return { status: 201, data: updatedCart };
   } catch (error) {
     console.error('Error in createCart:', error);
     return { status: 500, data: { error: 'Internal server error' } };
   }
+};
 
-  
+
+const getCart = async () => {
+  try {
+
+    const carts = await Cart.findAll({
+      include: [
+        {
+          model: Product,
+          as: 'products',
+          attributes: ['id'],
+        },
+      ],
+    });
+    
+    return carts;
+
+  } catch (error) {
+    console.error('Error in getCart:', error);
+    throw error; // Error al handler
+  }
 };
 
 module.exports = { createCart, getCart };

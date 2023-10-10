7680885d8abb982fe35536db5e9447d92280270b
Kobi96 <tobiasolveira@outlook.com>
2023-10-10 12:14:27 -0300

    localStorage mock

--- client/src/components/CartProductCard/CartProductCard.jsx
+++ client/src/components/CartProductCard/CartProductCard.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deleteCartProduct, postCartProduct } from "../../redux/actions";
 import style from "./CartProductCard.module.css";
 
 const CartProductCard = (props) => {
   const dispatch = useDispatch();
-
   const deleteProduct = () => {
     dispatch(deleteCartProduct(props.id));
   };
@@ -13,6 +12,7 @@ const CartProductCard = (props) => {
   const postProduct = () => {
     dispatch(postCartProduct(props.id));
   };
+
   return (
     <div className={style.divCard}>
       <p className={style.prop}>

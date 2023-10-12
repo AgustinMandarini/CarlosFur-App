15754cdf1739cb770d53e48c95338793a8aa96d1
RMGeorgetti <rodrigogeorgetti.cba@gmail.com>
2023-10-12 12:25:10 -0300

    agregado de putCart

--- server/src/routes/cartRoutes.js
+++ server/src/routes/cartRoutes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { createCartHandler } = require("../handlers/CartHandlers/createCartHandler");
 const { getCartHandler } = require("../handlers/CartHandlers/getCartHandler");
+const { updateCartHandler } = require("../handlers/CartHandlers/updateCartHandler");
 const {mpCartHandler} = require ("../handlers/MercadoPagoHandlers/mpCartHandler");
 const { deleteCartHandler } = require("../handlers/CartHandlers/deleteCartHandler");
 
@@ -8,6 +9,7 @@ const cartRouter = Router();
 
 cartRouter.get("/", getCartHandler);
 cartRouter.post("/", createCartHandler);
+cartRouter.put("/:cartId", updateCartHandler);
 cartRouter.delete("/:cartId", deleteCartHandler)
 cartRouter.post("/create_preference", mpCartHandler);
 // cartRouter.get("/:userId", getCartByUserIdHandler); // Falta controller

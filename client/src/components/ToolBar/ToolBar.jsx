5cd95dcc400a83a88826b4b618f9e8acd6764ee7
javimana <jpereyra1979@gmail.com>
2023-10-14 00:44:00 -0300

    FiltrosBack

--- client/src/components/ToolBar/ToolBar.jsx
+++ client/src/components/ToolBar/ToolBar.jsx
@@ -1,11 +1,8 @@
 // ToolBar.jsx
-import React from "react";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import {
@@ -16,16 +13,17 @@ import {
   setSort,
 } from "../../redux/actions";
 import style from "./ToolBar.module.css";
-import filter from "./../../imagenes/filter.png"
+import filter from "./../../imagenes/filter.png";
+
 
 const ToolBar = () => {
   const location = useLocation();
+  const dispatch = useDispatch();
 
   const productTypeList = useSelector((state) => state.productType);
-  const materialList = useSelector((state) => state.materialState); // Nueva lista de materiales
+  const materialList = useSelector((state) => state.materialState);
   const productList = useSelector((state) => state.muebles);
   const coloresList = useSelector((state) => state.colorState);
-
 
   const [showFilters, setShowFilters] = useState(false);
 
@@ -33,30 +31,20 @@ const ToolBar = () => {
     setShowFilters(!showFilters);
   };
 
-  // Armado de opciones por tipo de producto
-
   const productTypeNames = productTypeList.map(
     (productType) => productType.name
   );
-
-  const materialNames = materialList.map(
-    // Obtener nombres de materiales
-    (material) => material.name
-  );
+  const materialNames = materialList.map((material) => material.name);
 
   const productPrices = productList.map((product) => product.price);
-
-  const sortedProductPrices = productPrices.sort(function (a, b) {
-    return a - b;
-  });
+  const sortedProductPrices = [...productPrices].sort((a, b) => a - b);
 
   const chunkSize = Math.ceil(sortedProductPrices.length / 3);
+  const chunk = (arr, size) => arr.slice(0, size);
 
-  const cheapPrices = sortedProductPrices.slice(0, chunkSize);
-  const middlePrices = sortedProductPrices.slice(chunkSize, 2 * chunkSize);
-  const highPrices = sortedProductPrices.slice(2 * chunkSize);
-
-  const dispatch = useDispatch();
+  const cheapPrices = chunk(sortedProductPrices, chunkSize);
+  const middlePrices = chunk(sortedProductPrices.slice(chunkSize), chunkSize);
+  const highPrices = chunk(sortedProductPrices.slice(2 * chunkSize), chunkSize);
 
   const setSortProductsHandler = (event) => {
     dispatch(setSort(event.target.value));
@@ -71,8 +59,6 @@ const ToolBar = () => {
   };
 
   const setFilterByMaterialHandler = (event) => {
-    // Nuevo handler para filtrar por material
-    console.log(event.target.value);
     dispatch(setMaterial(event.target.value));
   };
 
@@ -84,7 +70,6 @@ const ToolBar = () => {
     <div className={style.cntnToolBar}>
       {location.pathname === "/home" && (
         <div className={style.container}>
-
           <div className={style.divSelect}>
             <Form.Select
               onChange={setFilterByProductTypeHandler}
@@ -93,107 +78,37 @@ const ToolBar = () => {
             >
               <option value="allProductTypes">Tipo de ambiente</option>
               {productTypeNames &&
-                productTypeNames.map((productType, index) => {
-                  return (
-                    <option value={productType} key={index}>
-                      {productType}
-                    </option>
-                  );
-                })}
+                productTypeNames.map((productType, index) => (
+                  <option value={productType} key={index}>
+                    {productType}
+                  </option>
+                ))}
             </Form.Select>
+          </div>
 
           <div className={style.filterIcon} onClick={toggleFilters}>
-          <span>Filtrados</span>
-           <img src={filter} alt="" className={style.filter}/>
-
+            <span>Filtrados</span>
+            <img src={filter} alt="" className={style.filter} />
           </div>
           {showFilters && (
             <div className={style.filterOptions}>
-              <div className={style.divSelect}>
-                <Form.Select
-                  size="sm"
-                  onChange={setSortProductsHandler}
-                  className={style.select}
-                >
-                  <option value="notSorted">Ordenar...</option>
-                  <option value="MC">Mas caros</option>
-                  <option value="MB">Mas baratos </option>
-                  <option value="MV">Mayor Antiguedad</option>
-                  <option value="MN">Menor Antiguedad </option>
-                </Form.Select>
-              </div>
+              {/* ... Otro código ... */}
 
               <div className={style.divSelect}>
                 <Form.Select
-                  onChange={setFilterByProductTypeHandler}
+                  onChange={setFilterByMaterialHandler}
                   size="sm"
                   className={style.select}
                 >
-                  <option value="allProductTypes">Tipo de ambiente</option>
-                  {productTypeNames &&
-                    productTypeNames.map((productType, index) => {
-                      return (
-                        <option value={productType} key={index}>
-                          {productType}
-                        </option>
-                      );
-                    })}
+                  <option value="allMaterials">Todos los Materiales</option>
+                  {materialNames &&
+                    materialNames.map((material, index) => (
+                      <option value={material} key={index}>
+                        {material}
+                      </option>
+                    ))}
                 </Form.Select>
               </div>
-
-
-          <div className={style.divSelect}>
-            <Form.Select
-              onChange={setFilterByMaterialHandler} // Nuevo evento para filtrar por material
-              size="sm"
-              className={style.select}
-            >
-              <option value="allMaterials">Todos los Materiales</option>
-              {materialNames &&
-                materialNames.map((material, index) => {
-                  return (
-                    <option value={material} key={index}>
-                      {material}
-                    </option>
-                  );
-                })}
-            </Form.Select>
-          </div>
-
-          <div className={style.divSelect}>
-            <Form.Select
-              onChange={setFilterByPriceHandler}
-              size="sm"
-              className={style.select}
-            >
-              <option value={[]}>Precios</option>
-              <option value={cheapPrices}>
-                Precios entre {cheapPrices[0]} y{" "}
-                {cheapPrices[cheapPrices.length - 1]}
-              </option>
-              <option value={middlePrices}>
-                Precios entre {middlePrices[0]} y{" "}
-                {middlePrices[middlePrices.length - 1]}
-              </option>
-              <option value={highPrices}>
-                Precios entre {highPrices[0]} y{" "}
-                {highPrices[highPrices.length - 1]}
-              </option>
-            </Form.Select>
-          </div>
-          <div className={style.divSelect}>
-            <Form.Select
-              size="sm"
-              onChange={setSortProductsHandler}
-              className={style.select}
-            >
-              <option value="notSorted">Ordenar...</option>
-              <option value="DESC">Mas caros</option>
-              <option value="MB">Mas baratos </option>
-              {/* <option value="MV">Mayor Antiguedad</option>
-              <option value="MN">Menor Antiguedad </option> */}
-            </Form.Select>
-          </div>
 
               <div className={style.divSelect}>
                 <Form.Select
@@ -233,9 +148,24 @@ const ToolBar = () => {
                   </option>
                 </Form.Select>
               </div>
+
+              <div className={style.divSelect}>
+                <Form.Select
+                  size="sm"
+                  onChange={setSortProductsHandler}
+                  className={style.select}
+                >
+                  <option value="notSorted">Ordenar...</option>
+                  <option value="DESC">Mas caros</option>
+                  <option value="ASC">Mas baratos </option>
+                  {/* <option value="MV">Mayor Antiguedad</option>
+              <option value="MN">Menor Antiguedad </option> */}
+                </Form.Select>
+              </div>
+
+              {/* ... Otro código ... */}
             </div>
           )}
-
         </div>
       )}
     </div>

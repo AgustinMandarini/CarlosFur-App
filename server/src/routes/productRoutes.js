425bb1f766c09af2f251de7dd05fc06fd32e4339
RMGeorgetti <rodrigogeorgetti.cba@gmail.com>
2023-10-14 19:15:26 -0300

    delete product terminado

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -3,11 +3,13 @@ const { getProductHandler } = require("../handlers/ProductHandlers/getProductHan
 const { getProductByIdHandler } = require('../handlers/ProductHandlers/getProductByIdHandler');
 const { postProductHandler } = require("../handlers/ProductHandlers/postProductHandler");
 const { putProductHandler } = require("../handlers/ProductHandlers/putProductHandler");
+const { deleteProductHandler } = require("../handlers/ProductHandlers/deleteProductHandler");
 
 const productRouter = Router();
 productRouter.get("", getProductHandler);
 productRouter.get("/:id", getProductByIdHandler);
 productRouter.post("", postProductHandler);
 productRouter.put("/:id", putProductHandler);
+productRouter.delete("/:productId", deleteProductHandler);
 
 module.exports = productRouter;

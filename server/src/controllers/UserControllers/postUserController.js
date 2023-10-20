a32e87d2c91797e7b2343078d80df3053cf8f5ab
Chwaksen <agustinmandarini47@gmail.com>
2023-10-19 21:43:06 -0300

    Primera parte del reset de password terminada. Se envia el email con el link de reseteo. Falta crear el formulario de nueva password y la ruta que valida el token y crea la nueva password

--- server/src/controllers/UserControllers/postUserController.js
+++ server/src/controllers/UserControllers/postUserController.js
@@ -2,6 +2,7 @@
 const { User } = require("../../db");
 const { encryptPassword } = require("../Utils/passwordUtils");
 const { nodeMailerConfig } = require("../Utils/nodeMailerConfig");
+const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 const createUser = async (
   user_name,
@@ -12,22 +13,27 @@ const createUser = async (
   auth0
 ) => {
   let hashedPassword = null;
-  if (password) hashedPassword = await encryptPassword(password);
+  if (password) {
+    hashedPassword = await encryptPassword(password);
+  } else {
+    // Si intenta crear un usuario sin password es porque ingresa desde google, entonces asigna el TOKEN_SECRET como clave default
+    hashedPassword = await encryptPassword(TOKEN_SECRET);
+  }
 
   try {
     // Crea un nuevo usuario en la base de datos
     const newUser = await User.create({
       user_name,
-      password: auth0 ? "123456" : hashedPassword, // Si intenta ingresar un usuario de google por formulario, le asignara esa clave que nunca podra ser validada
+      password: hashedPassword,
       e_mail,
       first_name,
       last_name,
       is_admin: false, // Siempre tiene false porque nadie puede ser admin
       enabled_user: true, // Esto es para cuando lo querés banear
     });
-    // Esta funcion envia un mail de bienvenida al email del nuevo usuario. El tercer argumento "wellcome", define el tipo de email
+    // Esta funcion envia un mail de bienvenida al email del nuevo usuario. El tercer argumento, define el tipo de email
     // que sera enviado
-    await nodeMailerConfig(e_mail, user_name, "wellcome");
+    await nodeMailerConfig(e_mail, user_name, (emailType = "welcome"));
     // Devuelve el nuevo usuario creado
     return {
       user_name,
--- server/src/controllers/Utils/nodeMailerConfig.js
+++ server/src/controllers/Utils/nodeMailerConfig.js
@@ -7,8 +7,13 @@ const gmailUser = process.env.MAILING_AUTH_USER;
 const gmailAppPassword = process.env.MAILING_AUTH_PASSWORD;
 
 // La funcion recibe e_mail y user_name como valores del usuario, y emailType como tipo de email que
-// sera enviado, que podra ser wellcome, newPassword o orderReceipt
-const nodeMailerConfig = async (e_mail, user_name, emailType) => {
+// sera enviado, que podra ser welcome, newPassword o orderReceipt
+const nodeMailerConfig = async (
+  e_mail,
+  user_name,
+  emailType = "welcome",
+  resetPassURL
+) => {
   // Configuracion de mail para enviar al usuario recien creado
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -28,12 +33,12 @@ const nodeMailerConfig = async (e_mail, user_name, emailType) => {
   // Obtén la ruta absoluta al directorio que contiene nodeMailerConfig.js
   const directorioActual = path.dirname(__filename);
 
-  if (emailType === "wellcome") {
-    // Construye la ruta absoluta al archivo wellcomeEmail.html
+  if (emailType === "welcome") {
+    // Construye la ruta absoluta al archivo welcomeEmail.html
     const rutaAlEmailTemplate = path.join(
       directorioActual,
       "emailTemplates",
-      "wellcomeEmail.html"
+      "welcomeEmail.html"
     );
     const source = fs.readFileSync(rutaAlEmailTemplate, "utf-8").toString();
     const template = handlebars.compile(source);
@@ -42,11 +47,25 @@ const nodeMailerConfig = async (e_mail, user_name, emailType) => {
     };
     htmlToSend = template(replacements);
   }
+  if (emailType === "newPassword") {
+    // Construye la ruta absoluta al archivo welcomeEmail.html
+    const rutaAlEmailTemplate = path.join(
+      directorioActual,
+      "emailTemplates",
+      "newPasswordEmail.html"
+    );
+    const source = fs.readFileSync(rutaAlEmailTemplate, "utf-8").toString();
+    const template = handlebars.compile(source);
+    const replacements = {
+      resetPassURL: resetPassURL,
+    };
+    htmlToSend = template(replacements);
+  }
 
   await transporter.sendMail({
     from: `"MSC Amoblamientos 👻" ${gmailUser}`,
     to: e_mail,
-    subject: "Bienvenido a MSC Amoblamientos!",
+    subject: "MSC Amoblamientos",
     html: htmlToSend,
   });
 };

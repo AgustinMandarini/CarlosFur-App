c13fe334d8ce6da0c03e385b9cd6478838263747
Chwaksen <agustinmandarini47@gmail.com>
2023-10-23 23:23:03 -0300

    Finalizado email de order, junto con ruta post Order. Queda refinar detalles (se hacen 4 ordenes simultaneas)

--- server/src/controllers/OrderController/createOrderController.js
+++ server/src/controllers/OrderController/createOrderController.js
@@ -1,11 +1,13 @@
 const { Order } = require("../../db");
+const { PaymentType } = require("../../db");
 const getCartById = require("../../controllers/CartController/getCartByIdController");
+const { nodeMailerConfig } = require("../Utils/nodeMailerConfig");
 
 const createOrder = async (req, res) => {
   console.log("ENTRA A LA RUTA CREATEORDER??");
   try {
-    const { collection_id, cartId, paymentTypeId } = req.body;
-    console.log("HAY COLL Y CART? : " + collection_id, cartId);
+    const { collection_id, cartId, payment_type, e_mail } = req.body;
+    console.log("HAY COLL Y CART? : " + collection_id, cartId, payment_type);
 
     if (!collection_id) {
       return res
@@ -21,11 +23,16 @@ const createOrder = async (req, res) => {
       return res.status(404).json({ error: "Cart not found" });
     }
 
+    //Busca si existe un paymentType
+    const paymentTypeObj = await PaymentType.findOrCreate({
+      where: { name: payment_type },
+    });
+
     const newOrder = await Order.create({
       mercadoPagoId: collection_id,
-      saleDate,
-      cartId,
-      paymentTypeId
+      saleDate: saleDate,
+      cartId: cartId,
+      paymentTypeId: paymentTypeObj[0].id,
     });
 
     const cartData = {
@@ -36,18 +43,33 @@ const createOrder = async (req, res) => {
         name: product.name,
         product_quantity: product.product_quantity,
         productId: product.productId,
+        price: product.price,
       })),
     };
     // console.log("holis", cartData)
 
     const orderWithCart = {
       ...newOrder.toJSON(),
+      payment_type: payment_type,
+      e_mail: e_mail,
       cartInfo: cartData,
     };
 
-
+    console.log("ORDERWITHCART: " + JSON.stringify(orderWithCart));
+    console.log("DATE: " + orderWithCart.saleDate);
     console.log("ORDERWITHCART: " + orderWithCart);
-
+    console.log("USER: " + orderWithCart.cartInfo.user_name);
+    orderWithCart.cartInfo.products.forEach((prod) => {
+      console.log("\t-" + prod);
+    });
+    console.log("TOTAL: " + orderWithCart.cartInfo.total_amount);
+    await nodeMailerConfig(
+      e_mail,
+      (user_name = orderWithCart.cartInfo.user_name),
+      (emailType = "orderReceipt"),
+      (resetPassURL = null),
+      orderWithCart
+    );
     return res.status(201).json(orderWithCart);
   } catch (error) {
     console.error("Error creating order:", error);

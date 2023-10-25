4a4673df16d99e9a8d7801598cf6bfcf772a6799
Chwaksen <agustinmandarini47@gmail.com>
2023-10-25 15:16:20 -0300

    Modificado index.js del server. alter ahora en false

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ alter: true, force: false }).then(() => {
+conn.sync({ alter: false, force: false }).then(() => {
   server.listen(3001, () => {
     console.log("Server listening at 3001");
   });
